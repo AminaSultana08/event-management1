32fa9e33df11a2cb0a21dd2e7049d9a5e7a02747
AminaSultana08 <aminasultana088@gmail.com>
2023-10-09 22:21:23 +0600

    environment varables added to firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAnLzEtIuBjp1K5XqoxuPNSrfb7DdjngCY",
-  authDomain: "event-management-auth-7cdee.firebaseapp.com",
-  projectId: "event-management-auth-7cdee",
-  storageBucket: "event-management-auth-7cdee.appspot.com",
-  messagingSenderId: "821190956979",
-  appId: "1:821190956979:web:f8ac1798040eb7dbe61dc6"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
